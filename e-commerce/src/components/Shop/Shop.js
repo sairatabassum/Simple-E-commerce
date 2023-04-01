4b6a9f2e91f9692ad9751630c2412382dec2948e
sairatabassum <sairatabassum386@gmail.com>
2023-04-01 23:42:48 +0600

    commit

--- e-commerce/src/components/Shop/Shop.js
+++ e-commerce/src/components/Shop/Shop.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Shop.css";
 
 const Shop = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("products.json")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
   return (
     <div className="shop-container">
       <div className="products-container">
-        <h1>This is for products</h1>
+        <h1>Length : {products.length}</h1>
       </div>
       <div className="cart-container">
         <h1>order summary</h1>
