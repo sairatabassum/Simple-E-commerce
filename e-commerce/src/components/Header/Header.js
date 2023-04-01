5a0a6b27fd0079b6640fcba40195d09dc4256441
sairatabassum <sairatabassum386@gmail.com>
2023-04-01 16:43:12 +0600

    commit

--- e-commerce/src/components/Header/Header.js
+++ e-commerce/src/components/Header/Header.js
@@ -1,7 +1,15 @@
 import React from "react";
+import "./Header.css";
+import logo from "../../images/Logo.svg";
 
 const Header = () => {
-  return <div>This is header</div>;
+  return (
+    <div>
+      <nav className="header-nav">
+        <img src={logo} alt="" />
+      </nav>
+    </div>
+  );
 };
 
 export default Header;
