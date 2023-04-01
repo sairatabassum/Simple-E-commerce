4df1e4cf109fabb08a44ca91a398e2ba559dc460
sairatabassum <sairatabassum386@gmail.com>
2023-04-01 18:46:11 +0600

    commit

--- e-commerce/src/components/Shop/Shop.js
+++ e-commerce/src/components/Shop/Shop.js
@@ -2,7 +2,16 @@ import React from "react";
 import "./Shop.css";
 
 const Shop = () => {
-  return <div></div>;
+  return (
+    <div className="shop-container">
+      <div className="products-container">
+        <h1>This is for products</h1>
+      </div>
+      <div className="cart-container">
+        <h1>order summary</h1>
+      </div>
+    </div>
+  );
 };
 
 export default Shop;
